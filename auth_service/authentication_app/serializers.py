13b55124996e411c34f9ffe159f2bcdca5cffb4f
Subject-Cupslock <ruso23159@gmail.com>
2025-09-01 09:34:33 +0300

    Исправлена  работа  сервиса для авторизации и регистрации - работает

--- auth_service/authentication_app/serializers.py
+++ auth_service/authentication_app/serializers.py
@@ -5,7 +5,10 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password']
-        read_only_fields = ['password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
     
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
